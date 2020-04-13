02e5dff6bc078ad43859aef11f1efe87ac0c5b1f
Andres Fernando <andres.fernando08@gmail.com>
2020-04-12 19:38:40 -0500

    creado header basico

---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+
+const header = props => (
+    <View style={styles.header}>
+        <Text style={styles.headerText}>{props.title}</Text>
+    </View>
+)
+
+const styles = StyleSheet.create({
+    header: {
+        marginTop: 25,
+        paddingVertical: 10,
+        alignItems: 'center',
+        backgroundColor: '#F01200'
+    },
+    headerText: {
+        color: '#fff',
+        // fontWeight: 'bold',
+        fontSize: 25
+    }
+})
+
+export default header
